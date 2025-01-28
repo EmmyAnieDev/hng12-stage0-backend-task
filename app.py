a0596f9aba18ea57ba2497ece81428738c4d7e44
EmmyAnieDev <Emaduilzjr1@gmail.com>
2025-01-28 22:40:01 +0100

    Initial commit for HNG12 Backend Task

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from datetime import datetime, timezone
+from flask_cors import CORS
+
+import config
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["GET"])
+def get_info():
+
+    email = config.EMAIL
+    github_url = config.GITHUB_URL
+
+    utc_now = datetime.now(timezone.utc).replace(microsecond=0).isoformat().replace("+00:00", "Z")
+
+    return jsonify({
+        "email": email,
+        "current_datetime": utc_now,
+        "github_url": github_url
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5001)
---
+++ config.py
@@ -0,0 +1,2 @@
+EMAIL = "emmanuelekwere19@gmail.com"
+GITHUB_URL = "https://github.com/EmmyAnieDev/hng12-stage0-backend-task"
